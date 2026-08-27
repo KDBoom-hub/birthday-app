0bbfc0412047093b5bf02757340b409bd84b92e8
Khanh Dao <hyperpig213455@gmail.com>
2026-08-27 16:47:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ name = st.text_input("Enter your name:", "Friend")
 bday_date  = st.date_input(
     "Select your birthday: ",
     min_value=datetime.date(2000, 1, 1),
-    max_value=datetime.today()
+    max_value=datetime.date.today()
 )
 
 # run calc when button is clicked
